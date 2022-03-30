35496f40f844ad9e1e9edd493144ccc05eda35da
aidan-vasquez <aidan-vasquez@codermerlin.codermerlin.com>
2022-03-30 13:54:53 -0500

    Files needed for IGIS and the base code

---
+++ Sources/ScenesShell/Background.swift
@@ -0,0 +1,15 @@
+import Scenes
+import Igis
+
+  /*
+     This class is responsible for rendering the background.
+   */
+
+
+class Background : RenderableEntity {
+
+      init() {
+          // Using a meaningful name can be helpful for debugging
+          super.init(name:"Background")
+      }
+}
---
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -0,0 +1,18 @@
+import Scenes
+
+  /*
+     This class is responsible for the interaction Layer.
+     Internally, it maintains the RenderableEntities for this layer.
+   */
+
+
+class InteractionLayer : Layer {
+
+      init() {
+          // Using a meaningful name can be helpful for debugging
+          super.init(name:"Interaction")
+
+          // We insert our RenderableEntities in the constructor
+
+      }
+  }

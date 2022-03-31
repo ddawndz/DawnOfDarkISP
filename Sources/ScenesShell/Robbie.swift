3e3aace32473838382ad386747865f514d43e2f5
William Hutchings <william.hutchings@student.allenisd.org>
2022-03-31 17:10:26 -0500

    testing again

--- Sources/ScenesShell/Robbie.swift
+++ Sources/ScenesShell/Robbie.swift
@@ -2,23 +2,23 @@ import Scenes
 import Igis
 
 class Robbie : RenderableEntity {
-
-    func renderRectangle(canvas: Canvas, rect: Rect, color: FillStyle, border: StrokeStyle) {
-        let currentRect = rect
-
-        let linewidth = Linewidth(linewith: 1)
-
-        canvas.render(currentRect, Linewidth, color, border)
-    }
-
     
-        
-
-
+    func renderRectangle(canvas: Canvas, rect: Rect, color: FillStyle, border: StrokeStyle) {
+        let rectangle = Rectangle(rect: rect, fillMode:.fillAndStroke)
+        let linewidth = LineWidth(width:1)
+        canvas.render(color, linewidth, border, rectangle)
+    }     
+    
+    let black = FillStyle(color:Color(.black))
+    let g = StrokeStyle(color:Color(.gray))
+    
     init() {
         super.init(name:"Robbie")
     }
-
     
+    override func setup(canvasSize:Size, canvas:Canvas) {
+        let rect = Rect(topLeft: Point(x:500, y:500), size:Size(width: 50, height: 50))
+        renderRectangle(canvas: canvas, rect: rect, color: black, border: g)
+    }
     
 }

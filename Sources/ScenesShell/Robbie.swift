1ce48512145751178fa5e9f1775fcda64d340488
William Hutchings <william.hutchings@student.allenisd.org>
2022-03-31 16:17:14 -0500

    testing github

--- Sources/ScenesShell/Robbie.swift
+++ Sources/ScenesShell/Robbie.swift
@@ -1,10 +1,23 @@
 import Scenes
 import Igis
-print("HI!")
+
 class Robbie : RenderableEntity {
+
+    func renderRectangle(canvas: Canvas, rect: Rect, color: FillStyle, border: StrokeStyle) {
+        let currentRect = rect
+
+        let linewidth = Linewidth(linewith: 1)
+
+        canvas.render(currentRect, Linewidth, color, border)
+    }
+
+        
+
+
     init() {
         super.init(name:"Robbie")
     }
+
     
     
 }

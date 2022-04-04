eed512bdd4173ba282bfdc299244f0420d83d211
aidan-vasquez <aidan-vasquez@codermerlin.codermerlin.com>
2022-04-04 16:30:32 -0500

    yes

--- Sources/ScenesShell/Aidan.swift
+++ Sources/ScenesShell/Aidan.swift
@@ -11,6 +11,8 @@ class Aidan : RenderableEntity {
         canvas.render(fillStyle, rectangle)
     }
     
+    func Hitbox(canvas: Canvas, rect: Rect)
+    
     init() {
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Aidan")

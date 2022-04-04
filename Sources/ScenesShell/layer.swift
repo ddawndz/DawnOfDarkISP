c9c6c7588da03cd0f8248ac3329c1b9588b77b35
William Hutchings <william.hutchings@student.allenisd.org>
2022-04-04 15:44:57 -0500

    editing the background

--- Sources/ScenesShell/layer.swift
+++ Sources/ScenesShell/layer.swift
@@ -8,14 +8,29 @@ class bgLayer : RenderableEntity {
     let parallaxBackgrounds : [Image]
     var parallaxCurrentPoints : [Point]
 
+    let dark : Image
+    
     init(backgroundSpeed: Int) {
         guard let parallaxBackgroundURL = URL(string: "https://i.ibb.co/C09XGhG/1920x1080-1.png") else {
             fatalError("Failed to create URL for parallaxBackground")
         }
+        guard let darkURL = URL(string: " ") else {
+            fatalError("failed to create URL for dark")
+        }
+        dark = Image(sourceURL: darkURL)
+        
         parallaxBackgrounds = [Image(sourceURL: parallaxBackgroundURL), Image(sourceURL: parallaxBackgroundURL)]
         parallaxCurrentPoints = [Point(), Point()]
         self.backgroundSpeed = backgroundSpeed
 
         super.init(name:"bgLayer")
-    }    
+    }
+    override func setup(canvasSize: Size, canvas: Canvas) {
+        canvas.setup(dark)
+    }
+    override func render(canvasSize: Size, canvas: Canvas) {
+        if dark.isReady {
+            canvas.render(dark)
+        }
+    }
 }

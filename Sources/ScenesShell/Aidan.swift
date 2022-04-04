37d21a65919644ecd03b437f7c74b65227197e8a
William Hutchings <william.hutchings@student.allenisd.org>
2022-04-04 15:31:55 -0500

    fixing home screen

--- Sources/ScenesShell/Aidan.swift
+++ Sources/ScenesShell/Aidan.swift
@@ -17,7 +17,6 @@ class Aidan : RenderableEntity {
     }
     override func setup(canvasSize:Size, canvas:Canvas) {
         // setting the size and rendering
-        let rectangle = Rect(topLeft: Point(x:200, y:200), size: Size(width: 200, height:200))
-        renderColorRect(canvas:canvas, rect: rectangle, R:255, G:100, B:100)
+
     }
 }

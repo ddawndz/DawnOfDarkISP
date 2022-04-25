d861e48b2ee083b2f9199dec80b51c8cdc74f494
William Hutchings <william.hutchings@student.allenisd.org>
2022-04-25 16:15:26 -0500

    dfsfad

--- Sources/ScenesShell/sprites.swift
+++ Sources/ScenesShell/sprites.swift
@@ -8,7 +8,7 @@ import Igis
 class Sprites : RenderableEntity {
 
     let mainSprites : Image
-    //let black : Image
+    let black : Image
     var count = 0
     var maxCount = 8
     var swap = 0
@@ -27,20 +27,20 @@ class Sprites : RenderableEntity {
             fatalError("Failed to create URL for whitehouse")
         }
         // https://linkpicture.com/q/Download8580.png
-        //guard let blackURL = URL(string:"https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500.png") else {
-        //    fatalError("Failed to create URL for black")
-        //}
+        guard let blackURL = URL(string:"https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500.png") else {
+            fatalError("Failed to create URL for black")
+        }
         //
 
         mainSprites = Image(sourceURL:mainSpritesURL)
-        //black = Image(sourceURL:blackURL)
+        black = Image(sourceURL:blackURL)
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Background")
     }
 
     override func setup(canvasSize:Size, canvas:Canvas) {
         canvas.setup(mainSprites)
-        //canvas.setup(black)
+        canvas.setup(black)
         xPos = canvasSize.center.x
         yPos = canvasSize.center.y
         //dispatcher.registerEntityMouseClickHandler(handler:self)
@@ -56,6 +56,7 @@ class Sprites : RenderableEntity {
      }
 
      */
+
     override func render(canvas:Canvas) {
         if downMove == true {
             current = "down"
@@ -131,7 +132,7 @@ class Sprites : RenderableEntity {
         }
 
 
-        /*
+        
         if black.isReady {
             let blackRect = Rect(topLeft:Point(x:50, y:50), size: Size(width:50, height: 50))
             let blackdestination = Rect(topLeft:Point(x:0, y:0), size:Size(width: Int.max, height: Int.max))
@@ -139,7 +140,7 @@ class Sprites : RenderableEntity {
             canvas.render(black)
             }
             
-         */
+         
 
         if mainSprites.isReady {
             let downRect = Rect(topLeft:Point(x:(64 * count) + 8, y:(17 * 11) + (46 * 10)), size:Size(width:56, height:56))
@@ -168,7 +169,7 @@ class Sprites : RenderableEntity {
 
 
             canvas.render(mainSprites)
-
+            
         }
     }
 

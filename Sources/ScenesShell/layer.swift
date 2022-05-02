b1fd7cd3780459a315576e8b5508bdebc22950fd
aidan-vasquez <aidan-vasquez@codermerlin.codermerlin.com>
2022-05-02 11:12:09 -0500

    yes

--- Sources/ScenesShell/layer.swift
+++ Sources/ScenesShell/layer.swift
@@ -12,27 +12,69 @@ class bgLayer : RenderableEntity {
     var ytot = 0
     var xtot = 0
     var time = 0
+    var xt = 0
+    var didRender = false
+    var screen = 1
     
-    func gRow(canvas:Canvas, obj: Image, columns: Int, yt: Int) {
+    func gRow(canvas:Canvas, obj: Image, columns: Int, xt: Int) {
         for i in 0 ... columns {
-            ytot = ys + yt * csy / 11 - csy / 11
-            obj.renderMode = .destinationRect(Rect(topLeft:Point(x:xtot, y: ytot), size:Size(width:csx / 21 + 1, height:csy / 11 + 1)))
-            xs = i * csx / 21
+            obj.renderMode = .destinationRect(Rect(topLeft:Point(x:csx / 21 * i + xt * csx / 21,y: ytot), size:Size(width:csx / 21 + 1, height:csy / 11 + 1)))
             
+            xtot = csx / 21 * i + xt * csx / 21
             canvas.render(obj)
         }
     }
-    func gGrid(canvas:Canvas, obj: Image, columns: Int, yt: Int, xt: Int, rows: Int) {
-        for i in 0 ... rows {
-            xtot = xt * csx / 21 - csx / 21 + columns * csx / 21
-            gRow(canvas:canvas, obj:obj, columns:columns, yt: yt)
-            obj.renderMode = .destinationRect(Rect(topLeft:Point(x:xt * csx / 21 - csx / 21, y: ys), size:Size(width:csx / 21, height:csy / 11)))
-            ys = i * csy / 11
 
-            let testx1 = xs + xt * csx / 21
-            let testy1 = ys + yt * csx / 21
-            print(testx1, testy1)
+    func gGrid(canvas:Canvas, obj: Image, columns: Int, xt: Int, yt: Int, rows: Int) {
+        for i in 0 ... rows {
+            obj.renderMode = .destinationRect(Rect(topLeft:Point(x:xtot ,y: csy / 11 * i + yt * csy / 11 - 1), size:Size(width:csx / 21 + 1, height:csy / 11 + 1)))
+            
+            ytot = csy / 11 * i + yt * csy / 11 - 1
+            
+            gRow(canvas:canvas, obj:obj, columns:columns, xt:xt)
         }
+    }
+
+    func rS1(canvas: Canvas) {
+        ground.renderMode = .destinationRect(Rect(topLeft:Point(x:0, y: 0), size:Size(width:csx / 21, height:csy / 11)))
+        print(csx / 21)
+        gGrid(canvas:canvas, obj: ground, columns: 21, xt: 0, yt: 0, rows: 11)
+
+
+        grass.renderMode = .destinationRect(Rect(topLeft:Point(x:0, y:0), size:Size(width: csx / 21, height:csy / 11)))
+        print(csx / 21)
+
+        gGrid(canvas:canvas, obj: grass, columns: 21, xt: 0, yt: 0, rows: 0)
+        gGrid(canvas:canvas, obj: grass, columns: 0, xt: 0, yt: 0, rows: 10)
+        gGrid(canvas:canvas, obj: grass, columns: 9, xt: 0, yt: 10, rows: 0)
+        gGrid(canvas:canvas, obj: grass, columns: 8, xt: 12, yt: 10, rows: 0)
+        gGrid(canvas:canvas, obj: grass, columns: 0, xt: 20, yt: 0, rows: 2)
+        gGrid(canvas:canvas, obj: grass, columns: 0, xt: 20, yt: 5, rows: 1)
+        gGrid(canvas:canvas, obj: grass, columns: 0, xt: 20, yt: 9, rows: 1)
+    }
+
+    func rS2(canvas: Canvas) {
+    }
+
+    func rS3(canvas: Canvas) {
+    }
+
+    func rS4(canvas: Canvas) {
+    }
+
+    func rS5(canvas: Canvas) {
+    }
+
+    func rS6(canvas: Canvas) {
+    }
+    
+    func rS7(canvas: Canvas) {
+    }
+
+    func rS8(canvas: Canvas) {
+    }
+
+    func rS9(canvas: Canvas) {
     }
     
     init() {
@@ -56,23 +98,52 @@ class bgLayer : RenderableEntity {
         csy = canvasSize.height
     }
     override func render(canvas:Canvas) {
-        if ground.isReady {
+        if ground.isReady && grass.isReady {
+            if let canvasSize = canvas.canvasSize, !didRender {
+                    // Clear the entire canvas
+                let clearRect = Rect(topLeft:Point(x:0, y:0), size:canvasSize)
+                let clearRectangle = Rectangle(rect:clearRect, fillMode:.clear)
+                canvas.render(clearRectangle)
+    
+                switch (screen) {
+                case 1:
+                    rS1(canvas:canvas)
+                case 2:
+                    rS2(canvas:canvas)
+                case 3:
+                    rS3(canvas:canvas)
+                case 4:
+                    rS4(canvas:canvas)
+                case 5:
+                    rS5(canvas:canvas)
+                case 6:
+                    rS6(canvas:canvas)
+                case 7:
+                    rS7(canvas:canvas)
+                case 8:
+                    rS8(canvas:canvas)
+                case 9:
+                    rS9(canvas:canvas)
+                default:
+                    fatalError("Unexpected pattern: \(screen)")
+                }
+                didRender = true
+            }
+        }
+        /*if ground.isReady {
             ground.renderMode = .destinationRect(Rect(topLeft:Point(x:0, y: 0), size:Size(width:csx / 21, height:csy / 11)))
 
-            gGrid(canvas:canvas, obj: ground, columns: 21, yt: 1, xt: 0, rows: 11)
+            gGrid(canvas:canvas, obj: ground, columns: 21, xt: 0, yt: 0, rows: 11)
 
             
         }
         if grass.isReady {
             grass.renderMode = .destinationRect(Rect(topLeft:Point(x:0, y:0), size:Size(width: csx / 21, height:csy / 11)))
 
-            gGrid(canvas:canvas, obj: grass, columns: 21, yt: 1, xt: 0, rows: 0)
-            gGrid(canvas:canvas, obj: grass, columns: 21, yt: 1, xt: 0, rows: 0)  
-            gGrid(canvas:canvas, obj: grass, columns: 21, yt: 1, xt: 0, rows: 0)
-            gGrid(canvas:canvas, obj: grass, columns: 21, yt: 11, xt: 0, rows: 0)
-            gGrid(canvas:canvas, obj: grass, columns: 0, yt: 2, xt: 0, rows: 10)
-            gGrid(canvas:canvas, obj:grass, columns: 1, yt: 2, xt: 20, rows: 10)
-            time = 0
-        }
+            gGrid(canvas:canvas, obj: grass, columns: 21, xt: 0, yt: 0, rows: 0)
+            gGrid(canvas:canvas, obj: grass, columns: 0, xt: 0, yt: 0, rows: 10)
+            gGrid(canvas:canvas, obj: grass, columns: 21, xt: 0, yt: 10, rows: 0)
+            gGrid(canvas:canvas, obj: grass, columns: 0, xt: 20, yt: 0, rows: 10)
+        }*/
     }
 }

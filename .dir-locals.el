8447cc636283382f2fcce7f06d6bfcac2aec9296
William Hutchings <william.hutchings@student.allenisd.org>
2022-05-09 22:01:53 -0500

    merge mistake

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((swift-mode . 
+  ((lsp-sourcekit-extra-args . (
+	 "-Xlinker" "-L" "-Xlinker" "The specified library path was not found '/usr/local/lib/merlin/SceneAnimations-0.1.5/SceneAnimations/.build/debug'" "-Xswiftc" "-I" "-Xswiftc" "The specified library path was not found '/usr/local/lib/merlin/SceneAnimations-0.1.5/SceneAnimations/.build/debug'" 
+
+)))))
